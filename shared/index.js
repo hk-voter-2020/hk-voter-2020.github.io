1980eb337674e1dd86d2ff722d5b7ab4585b315d
kbfl-campaign <kbfl-campaign@kbfl.org>
2020-04-02 07:55:19 +0800

    update

--- shared/index.js
+++ shared/index.js
@@ -1,3 +1,31 @@
+Chart.pluginService.register({
+    beforeDraw: function (chart) {
+        var width = chart.chart.width,
+            height = chart.chart.height,
+            ctx = chart.chart.ctx;
+        ctx.restore();
+        var fontSize = (height / 114).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#000";
+        var text = chart.config.options.elements.center.title,
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2;
+        ctx.fillText(text, textX, textY);
+
+        var fontSize = (height / 300).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#999";
+        var text = chart.config.options.elements.center.text,
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2 + (14* height / 114);
+        ctx.fillText(text, textX, textY);
+
+        ctx.save();
+    }
+});
+
 var heroChart = new Chart(document.getElementById('heroChart').getContext('2d'), {
     type: 'doughnut',
     data: {
@@ -21,6 +49,12 @@ var heroChart = new Chart(document.getElementById('heroChart').getContext('2d'),
       },
       legend: {
         display: false,
-      }
+      },
+      elements: {
+        center: {
+          title: '1,000',
+          text: '參與人數',
+        }
+      },
     }
 });
